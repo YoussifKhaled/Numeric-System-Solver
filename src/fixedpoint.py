cc13f54c957575684a71abc971d44670ddef0e7c
mohamed <moahmed.tarek2607@gmail.com>
2024-01-03 05:41:48 +0200

    merged phase 1 with phase 2

---
+++ src/fixedpoint.py
@@ -0,0 +1,38 @@
+import math
+from math import *
+
+def fixed_point(g, x0, precision, tol, max_iterations):
+
+  f = lambda x: eval(g, {"x":x,"e":math.e ,"sin":math.sin,"cos":math.cos,"sqrt":math.sqrt})
+  
+  relativeError = 1
+  count = 1
+  steps = []
+
+  while max_iterations != 0 and relativeError >= tol:
+
+    x0 = round(x0, precision - len(str(int(x0))))
+    x1 = f(x0)
+    x1 = round(x1, precision - len(str(int(x1))))
+
+    if x1 != 0:
+      relativeError = abs(x1 - x0) / abs(x1)
+
+    step = f"iteration number {count}: x = {x1},relativeError = {round(relativeError*100,5)}%"
+    steps.append(step)
+
+    x0 = x1
+    count += 1
+    max_iterations -= 1
+
+  if relativeError >= tol:
+    steps.clear()
+    steps.append("This system of equations will not converge")
+    return -1, steps
+
+  return x0, steps
+
+# result, steps = fixed_point("sin(sqrt(x))", 0.5, 6, 0.001, 100)
+# print(result)
+# for step in steps:
+#   print(step)
